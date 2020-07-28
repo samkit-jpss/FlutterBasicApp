cf8308e3eb76060f54adad13077b9e7e973310f0
SAMKIT SHAH <56602504+samkit-jpss@users.noreply.github.com>
2020-07-28 13:25:51 +0530

    profile card

--- home.dart
+++ home.dart
@@ -1,135 +1,143 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 myapp() {
-  var con = Container(
-    width: 300,
-    height: 300,
-    alignment: Alignment.bottomCenter,
-    child: Text(
-      "Welcome To Your Profile",
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 25,
-        fontWeight: FontWeight.bold,
-      ),
+  FlutterStatusbarcolor.setStatusBarColor(Colors.blue.shade900);
+  myprint(){
+    Fluttertoast.showToast(
+        msg: "Verified Profile",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.amber,
+        textColor: Colors.white,
+        
+        fontSize: 16.0
+    );
+  }
+  var myhome = Container(
+    alignment: Alignment.center,
+    width: double.infinity,
+    height: double.infinity,
+    margin: EdgeInsets.all(20),
+    color: Colors.blue.shade50,
+    child: Stack(
+      alignment: Alignment.topCenter,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: Colors.blue.shade900, width: 5),
+          ),
+          margin: EdgeInsets.all(50),
+          alignment: Alignment.center,
+          width: 350,
+          height: 200,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  
+
+                  Text(
+                "Samkit Shah ",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                width: 25,
+                height:25,
+                decoration: BoxDecoration(
+                  image:DecorationImage(image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQI0wgO6NJWRo-PzY69Qfqb6FVF6JdVRNhblg&usqp=CAU'),
+              fit: BoxFit.cover,
+              
+              ), 
+                )
+              )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.email,color: Colors.blue,size:15),
+                  Text("  shahsamkit35@gmail.com",style: TextStyle(color:Colors.black,
+                  ),),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.work,color: Colors.blue,size: 15,),
+                  Text("  Software Engineer",style: TextStyle(color:Colors.black),),
+                ],
+              ),
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: myprint,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(image: NetworkImage('https://github.com/samkit-jpss/FlutterBasicApp/raw/master/myimage.jpeg'),
+              fit: BoxFit.cover
+              ),
+             borderRadius: BorderRadius.circular(50),  
+             border: Border.all(
+               color:Colors.blue.shade900,
+               
+             )
+            ),
+            width: 100,
+            height: 100,
+          ),
+        ),
+      ],
+      
     ),
-    decoration: BoxDecoration(
-      image: DecorationImage(
-        fit: BoxFit.cover,
-        image: NetworkImage(
-            'https://github.com/samkit-jpss/FlutterBasicApp/blob/master/myimage.jpeg?raw=true'),
-      ),
-      borderRadius: BorderRadius.circular(200),
-      border: Border.all(width: 6, color: Colors.black),
-    ),
-  );
-  var myicon = Icon(
-    Icons.home,
-    size: 30,
   );
 
   return MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text("Samkit Shah"),
-              accountEmail: Text('shahsamkit35@gmail.com'),
-              currentAccountPicture: GestureDetector(
-                onTap: () {
-                  print("");
-                  Fluttertoast.showToast(
-                      msg: "This is your Account",
-                      toastLength: Toast.LENGTH_SHORT,
-                      timeInSecForIosWeb: 1,
-                      backgroundColor: Colors.black,
-                      textColor: Colors.white,
-                      fontSize: 16.0);
-                },
-                child: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      'https://github.com/samkit-jpss/FlutterBasicApp/blob/master/myimage2.jpeg?raw=true'),
-                ),
-              ),
-              otherAccountsPictures: <Widget>[
-                GestureDetector(
-                    onTap: () {
-                      print("");
-                      Fluttertoast.showToast(
-                          msg: "This is Other User",
-                          toastLength: Toast.LENGTH_SHORT,
-                          timeInSecForIosWeb: 1,
-                          backgroundColor: Colors.black,
-                          textColor: Colors.white,
-                          fontSize: 16.0);
-                    },
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          'https://img.pngio.com/microsoft-learning-personal-photo-png-200_200.png'),
-                    )),
-              ],
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                      'https://scx2.b-cdn.net/gfx/news/2018/studyofdista.jpg'),
-                ),
-              ),
-            ),
-            ListTile(
-              trailing: Icon(Icons.account_balance),
-              title: Text("My account"),
-            ),
-            ListTile(
-              trailing: Icon(Icons.payment),
-              title: Text("Payments"),
-            ),
-            ListTile(
-              title: Text("Check balance"),
-              trailing: Icon(Icons.account_balance_wallet),
-            ),
-            Divider(),
-            ListTile(
-              title: Text("Report"),
-              trailing: Icon(Icons.report_problem),
-            ),
-            ListTile(
-              title: Text("Feedback or Contact us"),
-              trailing: Icon(Icons.feedback),
-            ),
-          ],
-        ),
-      ),
-      backgroundColor: Colors.amber,
-      body: Center(child: con),
-      appBar: AppBar(
-        actions: <Widget>[myicon],
-        title: Text('My Data'),
-        backgroundColor: Colors.black,
-      ),
       bottomNavigationBar: CurvedNavigationBar(
-        color: Colors.white,
-        backgroundColor: Colors.amber,
-        buttonBackgroundColor: Colors.amber,
-        height: 50,
-        items: <Widget>[
-          Icon(Icons.verified_user, size: 20, color: Colors.black),
-          Icon(Icons.add, size: 20, color: Colors.black),
-          Icon(Icons.person, size: 20, color: Colors.black),
-          Icon(Icons.verified_user, size: 20, color: Colors.black),
-          Icon(Icons.list, size: 20, color: Colors.black),
-        ],
-        animationDuration: Duration(milliseconds:250),
+        buttonBackgroundColor: Colors.blue,
+        color: Colors.blue.shade900,
+        
+    backgroundColor: Colors.white,
+    items: <Widget>[
+          Icon(Icons.verified_user, size: 20, color: Colors.white),
+          Icon(Icons.add, size: 20, color: Colors.white),
+          Icon(Icons.person, size: 20, color: Colors.white),
+          Icon(Icons.exit_to_app, size: 20, color: Colors.white),
+          Icon(Icons.list, size: 20, color: Colors.white),
+        
+    ],
+            animationDuration: Duration(milliseconds:250),
        index: 2,
        animationCurve: Curves.fastOutSlowIn,
-        onTap: (index) {
-          debugPrint('Currnt screes is at index $index');
-        },
-      ),
+       height: 50,
+   
     ),
-    debugShowCheckedModeBanner: false,
+        body: myhome,
+        appBar: AppBar(
+          leading: Icon(Icons.menu,size:25),
+          actions: <Widget>[
+            Icon(Icons.home,size:25),
+            Text("   "),
+            Icon(Icons.open_in_new,size:25),
+            Text(" "),
+          ],
+          backgroundColor: Colors.blue.shade900,
+          title: Text("Business Profiles"),
+        )),
+  
   );
+  
 }
