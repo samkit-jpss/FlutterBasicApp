02896ed9615597812dca752380e85ed479b4c826
SAMKIT SHAH <56602504+samkit-jpss@users.noreply.github.com>
2020-08-29 10:56:29 +0530

    snackbar

--- main.dart
+++ main.dart
@@ -1,12 +1,73 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:navigation/red.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyApp(),
-    ),
-  );
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("SnackBar"),
+        ),
+        body: Layer(),
+      ),
+    );
+  }
+}
+
+class Layer extends StatefulWidget {
+  Layer({Key key}) : super(key: key);
+
+  @override
+  _LayerState createState() => _LayerState();
+}
+
+class _LayerState extends State<Layer> {
+  var p;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width:double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.all(110),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: RaisedButton(
+                onPressed: () {
+                  p = Scaffold.of(context).hasAppBar;
+                
+                  print(p);
+                },
+                child: Center(
+                  child: Text("Button for Scaffold.of()"),
+                ),
+              ),
+            ),
+            Center(
+              child: RaisedButton(
+                onPressed: () {
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("You pressed snackbar!"),
+                      duration: Duration(seconds: 3),
+                    ),
+                  );
+                },
+                child: Text("Snackbar"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
